60ea6c201efdb2ad8866e4e180579605dea9eced
Oleg <kovalyovop@yandex.ru>
2024-04-11 22:31:01 +0300

    Этап2 Тесты

---
+++ src/test/java/org/example/TasksRestControllerTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.example.repo.TaskRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class TasksRestControllerTest {
+
+    @Mock
+    TaskRepository taskRepository;
+
+    @Mock
+    MessageSource messageSource;
+
+    @InjectMocks
+    TasksRestController controller;
+
+    @Test
+    @DisplayName("GET /api/tasks returns HTTP response with status 200 ОК and list tasks")
+    void handleGetAllTasks_ReturnsValidResponseEntity() {
+        var tasks = List.of(new Task(UUID.randomUUID(), "TaskOne", false),
+                new Task(UUID.randomUUID(), "TaskTwo", true));
+
+        doReturn(tasks).when(this.taskRepository).findAll();
+
+        var responseEntity = this.controller.handleGetAllTasks();
+
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+        assertEquals(tasks, responseEntity.getBody());
+
+    }
+
+}
